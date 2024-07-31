896e7d968f9d950575ab728b612e859ccce3febb
S-Core21 <ogbonnashedrach8@gmail.com>
2024-07-31 15:33:56 +0100

    modified api url

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://godeyedb-6fcfc52eb9b0.herokuapp.com//users/'
+const apiUrl = 'https://godeyedb-6fcfc52eb9b0.herokuapp.com/users/'
 const refUrl = 'https://godeyedb-6fcfc52eb9b0.herokuapp.com/'
 
 
