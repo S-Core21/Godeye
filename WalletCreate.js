fb25edd65d3eff8329c3347f2d965c042cea7b19
S-Core21 <ogbonnashedrach8@gmail.com>
2024-08-08 05:06:06 +0100

    changes

--- WalletCreate.js
+++ WalletCreate.js
@@ -30,7 +30,7 @@ async function createWallet(ctx, userCache, chatID) {
       );
       const balanceInSol = balance / solanaWeb3.LAMPORTS_PER_SOL;
       ctx.reply(
-        `Your godeye wallet address: \n\`${keys.publicKey}\`\nSOl balance:  ${balanceInSol} SOL \n\nTips: We would never ask for your private key via phone, email, or text. Protect yourself from scams.`,
+        `👤 Your godeye wallet address: \n\`${keys.publicKey}\`\nSOl balance:  ${balanceInSol} SOL \n\n📝 Tips: We would never ask for your private key via phone, email, or text. Protect yourself from scams.`,
         {
           parse_mode: "Markdown",
           reply_markup: {
