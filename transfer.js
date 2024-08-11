92d9116f844e22e03ce6576e8632729f74bac986
S-Core21 <ogbonnashedrach8@gmail.com>
2024-08-12 00:00:03 +0100

    test pump fun

--- transfer.js
+++ transfer.js
@@ -45,19 +45,7 @@ async function transferMessage(webhookEvent, wallet, wallet2, sig, Source, solcA
           disable_web_page_preview: true,
         });
       } else {
-        if(ReceiverAcctData.nativeBalanceChange === 0){
-          const tmint = webhookEvent[0].tokenTransfers[0].mint;
-          const dexresult = await fetchData(tmint);
-          console.log('ca:', tmint)
-          const messageToSend = `${walletgroup(wallet.group)} ALERT\n*${wallet.name}* transferred *${formatNumber(desc[2])} ${desc[3]}*(${await soldollarvalue(tmint, desc[2])}) to *UNSAVED*\n\n*💡${dexresult.ticker} | MC: ${dexresult.mcap}*\n\`${tmint}\`\n🔎 *DYOR:* [SOLC](${sig}) | [X](${dexresult.twitter}) | [RICK](${dexresult.rick}) | [DS](${dexresult.Dexscreener}) | [DT](${dexresult.Dextools}) | [BE](${dexresult.Birdeye}) | [Pump](${dexresult.pump})\n\n🕵️‍♂️ Analyse Wallet: [W1](${AW1}${address1})\n\`${wallet.address}\` ➡️ [${wallet.name}](${solcAcct}${wallet.address}) \n🕵️‍♂️ Analyse Wallet: [W2](${AW1}${address2})\n\`${address2}\` ➡️ [UNSAVED](${solcAcct}${address2}) `
-          bot.telegram.sendMessage(user.chat_id, messageToSend, {
-            parse_mode: "Markdown",
-            disable_web_page_preview: true,
-            reply_markup: {
-              inline_keyboard: buyButtons(tmint),
-            },
-          });
-        }else if(ReceiverAcctData.nativeBalanceChange !== 0){
+        if(ReceiverAcctData.nativeBalanceChange !== 0){
           const quantitySol = ReceiverAcctData.nativeBalanceChange / 1000000000
           console.log(quantitySol)
           const quantitytoken = webhookEvent[0].tokenTransfers[0].tokenAmount 
@@ -72,6 +60,18 @@ async function transferMessage(webhookEvent, wallet, wallet2, sig, Source, solcA
                 inline_keyboard: buyButtons(tmint),
               },
             });
+        }else{
+          const tmint = webhookEvent[0].tokenTransfers[0].mint;
+          const dexresult = await fetchData(tmint);
+          console.log('ca:', tmint)
+          const messageToSend = `${walletgroup(wallet.group)} ALERT\n*${wallet.name}* transferred *${formatNumber(desc[2])} ${desc[3]}*(${await soldollarvalue(tmint, desc[2])}) to *UNSAVED*\n\n*💡${dexresult.ticker} | MC: ${dexresult.mcap}*\n\`${tmint}\`\n🔎 *DYOR:* [SOLC](${sig}) | [X](${dexresult.twitter}) | [RICK](${dexresult.rick}) | [DS](${dexresult.Dexscreener}) | [DT](${dexresult.Dextools}) | [BE](${dexresult.Birdeye}) | [Pump](${dexresult.pump})\n\n🕵️‍♂️ Analyse Wallet: [W1](${AW1}${address1})\n\`${wallet.address}\` ➡️ [${wallet.name}](${solcAcct}${wallet.address}) \n🕵️‍♂️ Analyse Wallet: [W2](${AW1}${address2})\n\`${address2}\` ➡️ [UNSAVED](${solcAcct}${address2}) `
+          bot.telegram.sendMessage(user.chat_id, messageToSend, {
+            parse_mode: "Markdown",
+            disable_web_page_preview: true,
+            reply_markup: {
+              inline_keyboard: buyButtons(tmint),
+            },
+          });
         }
       }
     } else if (!wallet && wallet2) {
@@ -82,21 +82,7 @@ async function transferMessage(webhookEvent, wallet, wallet2, sig, Source, solcA
           disable_web_page_preview: true,
         });
       } else {
-        if(senderAcctData.nativeBalanceChange === 0){
-          const tmint = webhookEvent[0].tokenTransfers[0].mint;
-          console.log('pumpp', tmint);
-  
-          const dexresult = await fetchData(tmint);
-  
-        const messageToSend = `${walletgroup(wallet2.group)} ALERT\n*UNSAVED* transferred *${formatNumber(desc[2])} ${desc[3]}*(${await soldollarvalue(tmint, desc[2])}) to *${wallet2.name}*\n\n*💡${dexresult.ticker} | MC: ${dexresult.mcap}*\n\`${tmint}\`\n🔎 *DYOR:* [SOLC](${sig}) | [X](${dexresult.twitter}) | [RICK](${dexresult.rick}) | [DS](${dexresult.Dexscreener}) | [DT](${dexresult.Dextools}) | [BE](${dexresult.Birdeye}) | [Pump](${dexresult.pump})\n\n🕵️‍♂️ Analyse Wallet: [W1](${AW1}${address1})\n\`${address1}\` ➡️ [UNSAVED](${solcAcct}${wallet2.address}) \n🕵️‍♂️ Analyse Wallet: [W2](${AW1}${address2})\n\`${wallet2.address}\` ➡️ [${wallet2.name}](${solcAcct}${wallet2.address}) `
-          bot.telegram.sendMessage(user.chat_id, messageToSend, {
-            parse_mode: "Markdown",
-            disable_web_page_preview: true,
-            reply_markup: {
-              inline_keyboard: buyButtons(tmint),
-            },
-          });
-        }else if(senderAcctData.nativeBalanceChange !== 0){
+        if(senderAcctData.nativeBalanceChange !== 0 && Source === 'SYSTEM_PROGRAM'){
           const quantitySol = senderAcctData.nativeBalanceChange / 1000000000
           console.log(quantitySol)
           const quantitytoken = webhookEvent[0].tokenTransfers[0].tokenAmount 
@@ -111,6 +97,20 @@ async function transferMessage(webhookEvent, wallet, wallet2, sig, Source, solcA
                 inline_keyboard: buyButtons(tmint),
               },
             });
+        }else{
+          const tmint = webhookEvent[0].tokenTransfers[0].mint;
+          console.log('pumpp', tmint);
+  
+          const dexresult = await fetchData(tmint);
+  
+        const messageToSend = `${walletgroup(wallet2.group)} ALERT\n*UNSAVED* transferred *${formatNumber(desc[2])} ${desc[3]}*(${await soldollarvalue(tmint, desc[2])}) to *${wallet2.name}*\n\n*💡${dexresult.ticker} | MC: ${dexresult.mcap}*\n\`${tmint}\`\n🔎 *DYOR:* [SOLC](${sig}) | [X](${dexresult.twitter}) | [RICK](${dexresult.rick}) | [DS](${dexresult.Dexscreener}) | [DT](${dexresult.Dextools}) | [BE](${dexresult.Birdeye}) | [Pump](${dexresult.pump})\n\n🕵️‍♂️ Analyse Wallet: [W1](${AW1}${address1})\n\`${address1}\` ➡️ [UNSAVED](${solcAcct}${wallet2.address}) \n🕵️‍♂️ Analyse Wallet: [W2](${AW1}${address2})\n\`${wallet2.address}\` ➡️ [${wallet2.name}](${solcAcct}${wallet2.address}) `
+          bot.telegram.sendMessage(user.chat_id, messageToSend, {
+            parse_mode: "Markdown",
+            disable_web_page_preview: true,
+            reply_markup: {
+              inline_keyboard: buyButtons(tmint),
+            },
+          });
         }
       }
     }
