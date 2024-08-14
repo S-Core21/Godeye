428334d66fd7ce5682aa5a327254661dc32afa6a
S-Core21 <ogbonnashedrach8@gmail.com>
2024-08-14 04:08:06 +0100

    test swap

--- index.js
+++ index.js
@@ -636,60 +636,42 @@ async function main() {
       console.log(Transferdescription);
       const desc = Transferdescription.split(" ");
       const address1 = desc[0];
-      const address2 = desc[5]?.replace(".", ""); 
-      userCache.forEach(async (user) => {
-        const wallet = user.wallets.find(
-          (wallet) => wallet.address === address1,
-        );
-        const wallet2 = user.wallets.find(
-          (wallet2) => wallet2.address === address2,
-        );
-        const sol = "So11111111111111111111111111111111111111112";
-        const AW1 = `https://dexcheck.ai/app/wallet-analyzer/`;
-        const signature = webhookEvent[0].signature;
-        const sig = `https://solscan.io/tx/${signature}`;
-        const solcAcct = "https://solscan.io/account/";
-        const solToken = "https://solscan.io/token/";
-        if (wallet || wallet2) {
-          const Type = webhookEvent[0].type;
-          const Source = webhookEvent[0].source;
-
-          if (Type === "SWAP") {
-            await swapMessage(webhookEvent, Source, wallet, desc, sol, AW1, sig, solcAcct, solToken, user, buyButtons, bot)
-            
-          } else if (Type === "TRANSFER") {
-           await transferMessage(webhookEvent, wallet, wallet2, sig, Source, solcAcct, desc, sol, AW1, bot, user, address1, address2, buyButtons)
-            
-          }else if(Type === 'BURN'){
-            const txid = webhookEvent[0].signature;
-            const txidLink = `https://solscan.io/tx/${txid}`;
-            const acctPrefix = "https://solscan.io/account/";
-            const tokenName = desc[3]?.replace(".", "")
-            const mint = webhookEvent[0].tokenTransfers[0].mint;
-            console.log(mint);
-            const dexresult = await fetchData(mint);
-
-            if(wallet){
-              const messageToSend = `${walletgroup(wallet.group)} ALERT \n🔥[BURN](${txidLink}) on ${Source.replace(/_/g, " ")} \n\`${wallet.address}\` (${wallet.name})\n\n🔹[${wallet.name}](${acctPrefix}${wallet.address}) *burned* ${formatNumber(desc[2])} ${tokenName}\n\n*🔗${desc[3]}(MC: $${formatMcap(dexresult.mcap)})*\nDYOR: [SOT](${dexresult.twitter}) |[DS](${dexresult.Dexscreener}) | [DT](${dexresult.Dextools}) | [PH](${dexresult.Photon})| [BE](${dexresult.Birdeye})| [Rick](${dexresult.rick})\nAnalyse Wallet: [W1](${AW1}${wallet})\n\`${mint}\` `;
-              bot.telegram.sendMessage(user.chat_id, messageToSend, {
-                parse_mode: "Markdown",
-                disable_web_page_preview: true,
-                reply_markup: {
-                  inline_keyboard: buyButtons(mint),
-                },
-              });
-            }
-        }else if(Type === 'TOKEN_MINT'){
+      const address2 = desc[5]?.replace(".", "");
+      try{
+        userCache.forEach(async (user) => {
+          const wallet = user.wallets.find(
+            (wallet) => wallet.address === address1,
+          );
+          const wallet2 = user.wallets.find(
+            (wallet2) => wallet2.address === address2,
+          );
+          const sol = "So11111111111111111111111111111111111111112";
+          const AW1 = `https://dexcheck.ai/app/wallet-analyzer/`;
+          const signature = webhookEvent[0].signature;
+          const sig = `https://solscan.io/tx/${signature}`;
+          const solcAcct = "https://solscan.io/account/";
+          const solToken = "https://solscan.io/token/";
+          if (wallet || wallet2) {
+            const Type = webhookEvent[0].type;
+            const Source = webhookEvent[0].source;
+  
+            if (Type === "SWAP") {
+              await swapMessage(webhookEvent, Source, wallet, desc, sol, AW1, sig, solcAcct, solToken, user, buyButtons, bot)
+              
+            } else if (Type === "TRANSFER") {
+             await transferMessage(webhookEvent, wallet, wallet2, sig, Source, solcAcct, desc, sol, AW1, bot, user, address1, address2, buyButtons)
+              
+            }else if(Type === 'BURN'){
               const txid = webhookEvent[0].signature;
               const txidLink = `https://solscan.io/tx/${txid}`;
               const acctPrefix = "https://solscan.io/account/";
               const tokenName = desc[3]?.replace(".", "")
               const mint = webhookEvent[0].tokenTransfers[0].mint;
               console.log(mint);
-              const dexresult = await tokenMintData(mint);
-
+              const dexresult = await fetchData(mint);
+  
               if(wallet){
-                const messageToSend = `${walletgroup(wallet.group)} ALERT \n[TOKEN MINT](${txidLink}) on ${Source.replace(/_/g, " ")} \n\`${wallet.address}\` (${wallet.name})\n\n🔹[${wallet.name}](${acctPrefix}${wallet.address}) *minted* ${formatNumber(desc[2])} ${tokenName}\n\n*🔗${dexresult.ticker}(MC: $${formatMcap(dexresult.mcap)})*\nDYOR: [SOT](${dexresult.twitter})| [PH](${dexresult.Photon})| [BE](${dexresult.Birdeye})| [Rick](${dexresult.rick})\nAnalyse Wallet: [W1](${AW1}${wallet})\n\`${mint}\` `;
+                const messageToSend = `${walletgroup(wallet.group)} ALERT \n🔥[BURN](${txidLink}) on ${Source.replace(/_/g, " ")} \n\`${wallet.address}\` (${wallet.name})\n\n🔹[${wallet.name}](${acctPrefix}${wallet.address}) *burned* ${formatNumber(desc[2])} ${tokenName}\n\n*🔗${desc[3]}(MC: $${formatMcap(dexresult.mcap)})*\nDYOR: [SOT](${dexresult.twitter}) |[DS](${dexresult.Dexscreener}) | [DT](${dexresult.Dextools}) | [PH](${dexresult.Photon})| [BE](${dexresult.Birdeye})| [Rick](${dexresult.rick})\nAnalyse Wallet: [W1](${AW1}${wallet})\n\`${mint}\` `;
                 bot.telegram.sendMessage(user.chat_id, messageToSend, {
                   parse_mode: "Markdown",
                   disable_web_page_preview: true,
@@ -698,22 +680,44 @@ async function main() {
                   },
                 });
               }
-          }else if(Type === 'NFT_MINT'){
-            await nftMintMessage(webhookEvent, desc, wallet, Source, bot, user)
-          }else if(Type === 'NFT_SALE'){
-              await nftSaleMessage(webhookEvent, desc, wallet, Source, bot, user)
-          }else if(Type === 'NFT_LISTING'){
-             await nftListMessage(webhookEvent, desc, wallet, Source, bot, user)
-          }else if(Type === 'NFT_CANCEL_LISTING'){
-            console.log('cancelleddddddd')
-             await nftCanListMessage(webhookEvent, desc, wallet, Source, bot, user)
-          }else if(Type === 'WITHDRAW_LIQUIDITY'){
-            await removeLiquidityMessage(webhookEvent, wallet, Source, AW1, bot, user)
-          }else if(Type === 'ADD_LIQUIDITY'){
-            await addLiquidityMessage(webhookEvent, wallet, Source, AW1, bot, user)
-          }// add new transaction types here 
-        }
-      });
+          }else if(Type === 'TOKEN_MINT'){
+                const txid = webhookEvent[0].signature;
+                const txidLink = `https://solscan.io/tx/${txid}`;
+                const acctPrefix = "https://solscan.io/account/";
+                const tokenName = desc[3]?.replace(".", "")
+                const mint = webhookEvent[0].tokenTransfers[0].mint;
+                console.log(mint);
+                const dexresult = await tokenMintData(mint);
+  
+                if(wallet){
+                  const messageToSend = `${walletgroup(wallet.group)} ALERT \n[TOKEN MINT](${txidLink}) on ${Source.replace(/_/g, " ")} \n\`${wallet.address}\` (${wallet.name})\n\n🔹[${wallet.name}](${acctPrefix}${wallet.address}) *minted* ${formatNumber(desc[2])} ${tokenName}\n\n*🔗${dexresult.ticker}(MC: $${formatMcap(dexresult.mcap)})*\nDYOR: [SOT](${dexresult.twitter})| [PH](${dexresult.Photon})| [BE](${dexresult.Birdeye})| [Rick](${dexresult.rick})\nAnalyse Wallet: [W1](${AW1}${wallet})\n\`${mint}\` `;
+                  bot.telegram.sendMessage(user.chat_id, messageToSend, {
+                    parse_mode: "Markdown",
+                    disable_web_page_preview: true,
+                    reply_markup: {
+                      inline_keyboard: buyButtons(mint),
+                    },
+                  });
+                }
+            }else if(Type === 'NFT_MINT'){
+              await nftMintMessage(webhookEvent, desc, wallet, Source, bot, user)
+            }else if(Type === 'NFT_SALE'){
+                await nftSaleMessage(webhookEvent, desc, wallet, Source, bot, user)
+            }else if(Type === 'NFT_LISTING'){
+               await nftListMessage(webhookEvent, desc, wallet, Source, bot, user)
+            }else if(Type === 'NFT_CANCEL_LISTING'){
+              console.log('cancelleddddddd')
+               await nftCanListMessage(webhookEvent, desc, wallet, Source, bot, user)
+            }else if(Type === 'WITHDRAW_LIQUIDITY'){
+              await removeLiquidityMessage(webhookEvent, wallet, Source, AW1, bot, user)
+            }else if(Type === 'ADD_LIQUIDITY'){
+              await addLiquidityMessage(webhookEvent, wallet, Source, AW1, bot, user)
+            }// add new transaction types here 
+          }
+        });
+      }catch(e){
+        console.log(e)
+      } 
     }
     res.sendStatus(200); // Respond to indicate the webhook was received
   });
