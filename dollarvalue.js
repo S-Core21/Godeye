6e4fc6d251c09eb9b202524bbb7e0f8f6e9ba6ec
S-Core21 <ogbonnashedrach8@gmail.com>
2024-08-18 02:09:13 +0100

    dollar value changes

--- dollarvalue.js
+++ dollarvalue.js
@@ -4,11 +4,20 @@ async function soldollarvalue(mint, value){
   try{
     const response = await axios.get(`https://price.jup.ag/v6/price?ids=${mint}`)
     const priceData = response.data
+    const response2 = await axios.get(`https://api.dexscreener.com/latest/dex/tokens/${Mint1}`)
+    const priceData2 = response2.data.pairs
     console.log(priceData)
-    const price = priceData.data[mint].price
+    if(priceData){
+      const price = priceData.data[mint].price
     const dollarValue = price * value
     const data = '$' + dollarValue.toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 2 })
     return data
+    }else if(priceData2){
+      const price = priceData2[0].priceUsd
+      const dollarValue = price * value
+    const data = '$' + dollarValue.toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 2 })
+    return data
+    }
   }catch(e){ 
     console.log('error fetching dollar value', e)
     const data = '$'
