3171345b1a4d5db9ac73f4afacdb153c7582979b
S-Core21 <ogbonnashedrach8@gmail.com>
2024-08-24 10:34:51 +0100

    editing feedbacks

--- index.js
+++ index.js
@@ -640,7 +640,7 @@ async function main() {
       console.log(Transferdescription);
       const desc = Transferdescription.split(" ");
       const address1 = webhookEvent[0].tokenTransfers.length > 0 ? webhookEvent[0].tokenTransfers[0].fromUserAccount : desc[0];
-      const address2 = webhookEvent[0].tokenTransfers.length > 0 ? webhookEvent[0].tokenTransfers[0].toUserAccount: desc[5].replace(/./g, " ");
+      const address2 = webhookEvent[0].tokenTransfers.length > 0 ? webhookEvent[0].tokenTransfers[0].toUserAccount: desc[5].replace(/\.$/, "");;
         userCache.forEach(async (user) => {
           const wallet = user.wallets.find(
             (wallet) => wallet.address === address1,
